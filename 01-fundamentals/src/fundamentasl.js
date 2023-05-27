be57a5d9bccd45f5b80697fbd74510e2b0ce5575
eduardovisa <eduardo.mvisa@gmail.com>
2023-05-27 13:27:09 -0600

    Change of project structure

---
+++ 01-fundamentals/src/fundamentasl.js
@@ -0,0 +1,34 @@
+// function Greeting() {
+//   return (
+//     <>
+//       <div className="someValue">
+//         <h2>Mi primer componente</h2>;
+//         <ul>
+//           <li>
+//             <a href="./index.js">Hi</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <h2>Hola mundo!</h2>
+//       <input type="text" name="" id="" />
+//     </>
+//   );
+// }
+
+// function Greeting() {
+//   return React.createElement('h2', {}, 'Hola mundo!');
+// }
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//   );
+// }
+
+// const Person = () => <h2>Eduardo</h2>;
+// const Message = () => {
+//   return <p>Este es mi mensaje </p>;
+// };
--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,41 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BookList from './book_list/book_list';
-
-// function Greeting() {
-//   return (
-//     <>
-//       <div className="someValue">
-//         <h2>Mi primer componente</h2>;
-//         <ul>
-//           <li>
-//             <a href="./index.js">Hi</a>
-//           </li>
-//         </ul>
-//       </div>
-//       <h2>Hola mundo!</h2>
-//       <input type="text" name="" id="" />
-//     </>
-//   );
-// }
-
-// function Greeting() {
-//   return React.createElement('h2', {}, 'Hola mundo!');
-// }
-
-// function Greeting() {
-//   return (
-//     <div>
-//       <Person />
-//       <Message />
-//     </div>
-//   );
-// }
-
-// const Person = () => <h2>Eduardo</h2>;
-// const Message = () => {
-//   return <p>Este es mi mensaje </p>;
-// };
+import BookList from './book_list';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
