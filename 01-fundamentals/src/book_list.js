6e1e35deafd62ae7d2d107d210fbcb4e655e5299
eduardovisa <eduardo.mvisa@gmail.com>
2023-05-30 15:42:29 -0600

    Adding JS to component

--- 01-fundamentals/src/book_list.js
+++ 01-fundamentals/src/book_list.js
@@ -12,12 +12,31 @@ const BookList = () => {
   );
 };
 
+// const Book = () => {
+//   return (
+//     <article className="book">
+//       <Image />
+//       <Title />
+//       <Author />
+//     </article>
+//   );
+// };
+
+const author = 'John Grisham';
 const Book = () => {
+  const title = 'The Boys from Biloxi: A Legal Thriller';
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/71N1ryT6iGL._AC_UL320_.jpg"
+        alt="book_image"
+      />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {/* <h4>{author.toUpperCase()}</h4> */}
+      {/* <p>{let x = 6}</p> */}
+      <p>{6 + 6}</p>
     </article>
   );
 };
@@ -32,7 +51,14 @@ const Image = () => (
 const Title = () => <h2>The Boys from Biloxi: A Legal Thriller</h2>;
 
 const Author = () => {
-  return <h4>John Grisham</h4>;
+  const inlineHeadinStyle = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  };
+
+  // return <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>John Grisham</h4>;
+  return <h4 style={inlineHeadinStyle}>John Grisham</h4>;
 };
 
 export default BookList;
